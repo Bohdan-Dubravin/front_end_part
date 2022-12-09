98e0f73a7b7b3a7ef6997768520835b025e8e8cf
Bohdan-Dubravin <dubravin111@gmail.com>
2022-12-09 07:58:09 +0200

    query

--- src/layouts/News.jsx
+++ src/layouts/News.jsx
@@ -1,23 +1,26 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import PostList from '../components/PostList'
-import Tags from '../components/Tags'
-import { getAllPosts } from '../redux/slices/postSlice'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import PostList from '../components/PostList';
+import Tags from '../components/Tags';
+import { getAllPosts } from '../redux/slices/postSlice';
 
 const News = () => {
-  const { posts, status } = useSelector((state) => state.post)
-  const dispatch = useDispatch()
-
+  const { posts, status } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+  const [searchParams, getSearchParams] = useSearchParams();
+  const tags = searchParams.get('tag');
+  console.log(tags);
   useEffect(() => {
-    dispatch(getAllPosts())
-  }, [dispatch])
+    dispatch(getAllPosts(tags));
+  }, [dispatch, tags]);
 
   return (
-    <div className='relative w-[100%] mt-[30px] flex'>
+    <div className="relative w-[100%] mt-[30px] flex">
       <PostList isLoading={status} posts={posts} />
       <Tags />
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
